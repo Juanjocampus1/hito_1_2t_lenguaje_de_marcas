e5dab112cbf0cc280043dbdd92b425920a4c8067
usuario1 <ejemplo@gmail.com>
2024-02-06 12:38:33 +0100

    decimonovena entrega

--- src/components/seacher.js
+++ src/components/seacher.js
@@ -1,12 +1,22 @@
 const searchInput = document.getElementById('searchInput');
 const searchButton = document.getElementById('searchButton');
-const textToSearch = document.getElementById('textToSearch');
+const textToSearch = document.querySelectorAll('.textToSearch');
 
-searchButton.addEventListener('click', function() {
+function searchAndHighlight() {
     const searchValue = searchInput.value;
     const searchRegExp = new RegExp(searchValue, 'gi');
-    
-    textToSearch.innerHTML = textToSearch.textContent.replace(searchRegExp, function(match) {
-        return `<span class="highlight">${match}</span>`;
+
+    textToSearch.forEach(function(element) {
+        element.innerHTML = element.textContent.replace(searchRegExp, function(match) {
+            return `<span class="highlight">${match}</span>`;
+        });
     });
+}
+
+searchButton.addEventListener('click', searchAndHighlight);
+
+searchInput.addEventListener('keypress', function(event) {
+    if (event.key === 'Enter') {
+        searchAndHighlight();
+    }
 });
